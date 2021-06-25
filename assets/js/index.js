a0b2f1956e89ec847693357658a0bd0f759b59d1
Gautier De Coninck <gautier.deconinck@gmail.com>
2021-06-25 09:44:48 +0200

    adjustement on css (filters part but not finished)

---
+++ assets/js/index.js
@@ -0,0 +1,37 @@
+// DOM Selector
+let optionWrapper = document.getElementsByClassName('option-wrapper');
+let dropdownContent = document.getElementsByClassName('dropdown-content');
+let filterName = document.getElementsByClassName('filter-name');
+
+/*class searchFilters{
+  constructor (optionWrapper, dropdownContent){
+    this.optionWrapper = optionWrapper;
+    this.dropdownContent = dropdownContent;
+  }
+}
+
+document.querySelector('#filters').forEach(elt => {
+  
+});*/
+// Deploy filters menu
+let ingredients = document.querySelector('#ingredients');
+ingredients.addEventListener('click', function () {
+  optionWrapper[0].classList.toggle('open');
+  dropdownContent[0].classList.toggle('open');
+  filterName[0].remove();
+  document.getElementById('ingredients-filter-button').insertAdjacentHTML('afterbegin', '<input type="text" placeholder="Recherche un ingrédient" id="ingredient-search" />');
+  document.getElementById('ingredient-search').focus();
+});
+
+document.querySelector('#appliances').addEventListener('click', function () {
+  optionWrapper[1].classList.toggle('open');
+  dropdownContent[1].classList.toggle('open');
+});
+document.querySelector('#ustensils').addEventListener('click', function () {
+  console.log(optionWrapper);
+  console.log(dropdownContent);
+  optionWrapper[2].classList.toggle('open');
+  dropdownContent[2].classList.toggle('open');
+});
+
+
